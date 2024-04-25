c90233ebd9ac17d31dc6291899154edb493aa090
Sarveshwaran15 <sivasarvesh02@gmail.com>
2024-04-25 10:45:37 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Sidebar from './componenets/Sidebar/Sidebar'
+import Main from './componenets/Main/Main'
+
+
+
+const App = () => {
+  return (<>
+  <Sidebar/>
+  <Main/></>
+  )
+}
+
+export default App
